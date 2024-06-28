6db2cd7add671d66328cc075f48b0ab4add9ade2
Laura Mendieta <laumendieta06@gmail.com>
2024-06-27 21:41:36 -0500

    [UPDATE] FUNDAMENTOS ANGULAR

---
+++ proyecto1/fundamentos/Angular/angularFundamentos/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavegationComponent} from './components/navegation/navegation.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavegationComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+// Componente principal
+export class AppComponent {
+  // Aqui va la logica del proyecto
+  title = 'angularFundamentos';
+}
---
+++ proyecto1/fundamentos/Angular/angularFundamentos/src/app/components/navegation/navegation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // Asi es como se exporta en el html
+  selector: 'app-navegation',
+  standalone: true,
+  imports: [],
+  templateUrl: './navegation.component.html',
+  styleUrl: './navegation.component.css'
+})
+
+export class NavegationComponent {
+
+}
