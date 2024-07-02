3c5c6210391891ed3736b8e2acb3a103f38f5deb
Laura Mendieta <laumendieta06@gmail.com>
2024-07-02 17:30:18 -0500

    [UPDATE] FINALIZADO CON DETALLES - SIN IMAGENES

--- proyecto1/FRONTEND/src/app/components/navegation/navegation.component.ts
+++ proyecto1/FRONTEND/src/app/components/navegation/navegation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navegation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navegation.component.html',
   styleUrl: './navegation.component.css'
 })
--- proyecto1/FRONTEND/src/app/pages/admin/admin.component.ts
+++ proyecto1/FRONTEND/src/app/pages/admin/admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
