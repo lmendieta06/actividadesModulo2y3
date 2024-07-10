3b7daabaad5de4f4d67a9ac50515e4101f3eec0b
Laura Mendieta <laumendieta06@gmail.com>
2024-07-10 02:54:55 -0500

    [UPDATE] PRODUCTOS RENDERIZADOS

--- proyecto1/FRONTEND/src/app/pages/products/products.component.ts
+++ proyecto1/FRONTEND/src/app/pages/products/products.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavegationComponent } from '../../components/navegation/navegation.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ChatBotComponent } from '../../components/chat-bot/chat-bot.component';
+import { ProductsService } from '../../services/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [NavegationComponent, FooterComponent, ChatBotComponent],
+  imports: [NavegationComponent, FooterComponent, ChatBotComponent, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  productsService = inject(ProductsService);
+
+  // Obtener la informacion al hacer la peticion get
+  // Aqui se guardaran todos los productos.
+  allProducts : any [] = [];
+
+  // Accede a las peticiones, y da una respuesta
+  obtenerProductos(){
+    // Subscribe es el que da la respuesta
+    this.productsService.getProducts().subscribe((res:any) =>{
+      if(res){
+        console.log(res);
+        // Guardar todo en allProducts
+        this.allProducts = res;
+      }else{
+        console.error("Hubo un error");
+      }
+    })
+  }
+
+  // Metodo que permite que se rendericen (muestren en pantalla) los productos con el sitio web
+
+  ngOnInit(){
+    // Decir que lo renderice
+    this.obtenerProductos();
+  }
 }
--- proyecto1/FRONTEND/src/app/services/products.service.ts
+++ proyecto1/FRONTEND/src/app/services/products.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,52 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   constructor() { }
+
+  httpClient = inject(HttpClient);
+
+  // Crear variables de entorno para cada peticion
+
+  API_URL_GET = "http://localhost:3000/obtenerProductos";
+  API_URL_POST = "http://localhost:3000/agregarProducto";
+  API_URL_PUT = "http://localhost:3000/actualizarProducto";
+  API_URL_DELETE = "http://localhost:3000/eliminarProducto";
+
+  // OBTENER DATOS
+  getProducts(){
+    // Hago la peticion get
+    return this.httpClient.get(this.API_URL_GET);
+  }
+
+  // CREAR DATOS
+  // Esta funcion recibe parametros
+  postProduct(nombre:string,  imagen:string, precioBase:number){
+    // Crear objeto que envia la informacion
+    const infoProducto = {
+      nombre:nombre,
+      imagen:imagen,
+      precioBase:precioBase
+    }
+
+    return this.httpClient.post(this.API_URL_POST, infoProducto);
+  }
+
+  // MODIFICAR DATOS
+  putProduct(nombre:string, imagen:string, precioBase:number, id:string ){
+    // Objeto que contiene la informacion a modificar
+    const infoProducto = {
+      nombre:nombre,
+      imagen:imagen,
+      precioBase:precioBase
+    }
+
+    return this.httpClient.put(`${this.API_URL_PUT}/${id}`, infoProducto);
+
+  }
+  // ELIMINAR DATOS
+
+  deleteProduct(id:string){
+
+    return this.httpClient.delete(`${this.API_URL_DELETE}/${id}`);
+  }
+  
 }
