76d63671c51d3acee356b251e07c2c67c8d7024c
Laura Mendieta <laumendieta06@gmail.com>
2024-07-30 12:57:46 -0500

    [CREATE] AUTENTICACION JWT

---
+++ proyecto2/BACKEND/src/lib/jwt.js
@@ -0,0 +1,45 @@
+// Dependencia que nos permite usar
+import jwt from "jsonwebtoken";
+// Configurar la contraseña de jws
+const secretKey = process.env.CONTRASENA_JWS;
+
+// Generar funciones de generar y verificar token
+// No son promesas, por lo que no necesitan ser async pero es mejor hacerlo para poder mejorar flujo de la informacion
+
+export function generateToken(payload){
+    // Se asegura de que lo que se cree al llamar la funcion sea asincronico
+    // LO que esta dentro de los primeros parentesis sera como un try catch
+    return new Promise((resolve, reject)=>{
+        // Para generar el token se necesita la clave, el payload, tiempo se expiracion de token
+        // Crear la firma
+        // El expire es la duracion del token, en este caso una hora
+        jwt.sign(payload, secretKey, {expiresIn: "1h"}, (token, error)=>{
+            // Manejo de errores en la funcion creada
+            // Validar si hay error al crear token
+
+            if(error){
+                reject(new Error("Error al generar jsonwebtoken", message.error));
+            }else{
+                resolve(token);
+            }
+        });
+    });
+}
+
+export function verifyToken(token){
+
+    // Tambien se crea como una promesa, para que maneje el flujo de usuarios
+
+    return new Promise((resolve, reject)=>{
+        jwt.verify(token, secretKey, (decoded, error)=>{
+            // Manejar errores
+            // Validar si se decodifico el token correctamente
+            
+            if(error){
+                reject(new Error("Error al decodificar jsonwebtoken", message.error));
+            }else{
+                resolve(decoded);
+            }
+        })
+    })
+}
